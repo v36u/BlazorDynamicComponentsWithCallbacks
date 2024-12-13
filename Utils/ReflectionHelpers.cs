2bf05b5dd07249748d73f5c1816fd58c6a193458
Vlad Dumitru <vlad.dumitru@tokero.dev>
2024-12-13 11:37:03 +0200

    Initial example

---
+++ Utils/ReflectionHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace BlazorDynamicComponentsWithCallbacks.Utils;
+
+public static class ReflectionHelpers
+{
+  public static Type? ResolveComponent(string componentNamespacePrefix, string componentName)
+  {
+    var componentAssembly = componentNamespacePrefix.Split('.').First();
+    var assembly = Assembly.Load(componentAssembly);
+
+    var res = assembly.GetTypes()
+            .Where(t =>
+                t.Namespace != null &&
+                t.Namespace.StartsWith(componentNamespacePrefix, StringComparison.InvariantCultureIgnoreCase))
+            .Where(t => t.Name.Equals(componentName, StringComparison.InvariantCultureIgnoreCase))
+            .FirstOrDefault();
+    return res;
+  }
+}
